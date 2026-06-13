6e9da9622e21668e2f28136939d05c5772d06259
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-06-13 08:39:39 +0100

    dev commit

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -9,7 +9,7 @@ import { fetchInvoicesPages } from "@/app/lib/data";
 
 export default async function Page(props: {searchParams?: Promise<{query?: string; page?: string}>}) {
   const searchParams = await props.searchParams
-  const query = searchParams?.query
+  const query = searchParams?.query || ''
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
 
